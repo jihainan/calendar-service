998d96722682b6d933253a3d92460adc9ffd279e
jihainan <alwaysbegoing@163.com>
2020-08-27 10:26:12 +0800

    add hello api

--- src/main/java/com/lark/calendar/CalendarServiceApplication.java
+++ src/main/java/com/lark/calendar/CalendarServiceApplication.java
@@ -1,13 +1,11 @@
 package com.lark.calendar;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
-
+/**
+ * @author fight
+ */
 @SpringBootApplication
 public class CalendarServiceApplication {
 
